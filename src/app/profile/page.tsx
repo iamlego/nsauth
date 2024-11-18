f5d0cde95b48a9b0973f96d8ba5371640ef8682b
iamlego <sairaj.jadhav812@gmail.com>
2024-11-17 18:06:41 -0800

    logout route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,30 @@
+"use client"
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 export default function ProfilePage() {
+    const router = useRouter()
+    const logout = async () => {
+        try {
+            await axios.get("/api/users/logout");
+            router.push("/login");
+        } catch (error: any) {
+            console.log(error.message);
+        }
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2" >
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
+            <hr />
+            <button 
+            onClick={logout}
+            className="bg-orange-500 mt-4 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+                Logout
+            </button>  
         </div>
     )
 }
