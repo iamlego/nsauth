80f6441afe2b16e509c425c1a2731b7cd957270d
iamlego <sairaj.jadhav812@gmail.com>
2024-11-18 23:26:41 -0800

    middleware

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -3,12 +3,12 @@ import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModel";
 import { connect } from "@/dbConfig/dbConfig";
 
-connect();
+connect(); 
 
 export async function GET(request:NextRequest){
     try {
         const userId = await getDataFromToken(request);
-        const user = User.findOne({_id: userId}).select("-password"); 
+        const user = await User.findOne({_id: userId}).select("-password"); 
         return NextResponse.json({
             message: "User",
             data:user
@@ -17,4 +17,4 @@ export async function GET(request:NextRequest){
     } catch (error:any) {
         return NextResponse.json({error: error.message}, {status:400});
     }
-}
+} 
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,11 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ProfilePage() {
     const router = useRouter()
+    const [data, setData] = useState("Nothing");
     const logout = async () => {
         try {
             await axios.get("/api/users/logout");
@@ -14,17 +16,33 @@ export default function ProfilePage() {
         }
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('api/users/me')
+        console.log(res.data.data.email);
+        setData(res.data.data.email);
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2" >
             <h1>Profile</h1>
             <hr />
             <p>Profile Page</p>
+            <h2 className="p-3 rounded bg-purple-300">
+                {data === 'Nothing' ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}
+            </h2>
+            {/* <h2>{ data == 'Nothing' ? 'Nothing' || <Link href={`/profile/${data}`}></Link> }</h2> */}
             <hr />
             <button 
             onClick={logout}
             className="bg-orange-500 mt-4 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
                 Logout
             </button>  
+            <button 
+            onClick={getUserDetails}
+            className="bg-green-500 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                Get User Details
+            </button>    
+
         </div>
     )
 }
